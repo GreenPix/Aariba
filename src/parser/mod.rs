308c85e3f93691e3613b9229a06dd6cacc0a43ce
Vaelden <maugan.villatel@gmail.com>
2015-11-21 18:37:50 +0000

    Reexport ParseError and ParseResult

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6,5 +6,7 @@ pub use self::parser::expression;
 pub use self::parser::expression_tree;
 pub use self::parser::rules_list;
 pub use self::parser::rules_evaluator;
+pub use self::parser::ParseError;
+pub use self::parser::ParseResult;
 pub use self::expressions::Node;
 pub use self::expressions::convert as convert_expr;
